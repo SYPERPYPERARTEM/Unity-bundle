cc7334c9e536f33afc618bec0ca5cd99e1fcd704
SYPERPYPERARTEM <melkaya45@gmail.com>
2026-08-01 04:06:53 +0300

    Update CreateAssetBundles.cs

--- Assets/Scenes/Assets/Editor/CreateAssetBundles.cs
+++ Assets/Scenes/Assets/Editor/CreateAssetBundles.cs
@@ -1,1 +1,10 @@
-
+using UnityEditor;
+using System.IO;
+public class CreateAssetBundles {
+    [MenuItem("Assets/Build AssetBundles")]
+    public static void BuildAllAssetBundles() {
+        string dir = "Assets/AssetBundles";
+        if (!Directory.Exists(dir)) Directory.CreateDirectory(dir);
+        BuildPipeline.BuildAssetBundles(dir, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows64);
+    }
+}
